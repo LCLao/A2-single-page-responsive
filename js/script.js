11116526d6f4b6b353108611146a294a9b5a309f
LCLao <N01493342@humbermail.ca>
2023-10-12 02:36:58 -0400

    added images, edited html css and js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("menu").classList.toggle("active"); 
+    // this will add an active class to the menu when clicked, which will show menu options
+});
